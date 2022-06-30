f370399faa92a13ecf78fb464a17480d36b71168
felixfjg <2432387060@qq.com>
2022-06-30 15:04:03 +0800

    fjg first commit

---
+++ src/main/java/com/example/springTest/SpringTestApplication.java
@@ -0,0 +1,25 @@
+package com.example.springTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@RestController
+public class SpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    @RequestMapping(value="/hello", method= RequestMethod.GET)
+    public String helloDemo(){
+        return "Hello my demo project";
+    }
+}
+
